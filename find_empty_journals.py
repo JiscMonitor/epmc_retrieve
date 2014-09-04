bdd152f36ce33f852aa84cfa5cebf59481abfa83
Steven Eardley <me@steveneardley.co.uk>
2014-09-04 19:40:31 +0100

    Now retrieves the raw XML for all empty journals. (currently restricted to 100).

--- find_empty_journals.py
+++ find_empty_journals.py
@@ -2,41 +2,45 @@
 
 import settings, requests, json
 
-SEARCH = settings.ES_INDEX + "/_search?pretty"
-RES_SIZE = 1000
+def find_empty_journals():
 
-query = \
-{
-    'fields' : ['index.issn'],
-    'query' : { 'match_all' : {} },
-    'size' : RES_SIZE
-}
+    SEARCH = settings.ES_INDEX + "/_search?pretty"
+    RES_SIZE = 1000
 
-res_from = 0
-keep_looping = True
-j_issns = []
-a_issns = []
+    query = \
+    {
+        'fields' : ['index.issn'],
+        'query' : { 'match_all' : {} },
+        'size' : RES_SIZE
+    }
 
-while keep_looping:
-    query['from'] = res_from
-    resp = requests.get(SEARCH, data=json.dumps(query))
-    results = resp.json()['hits']['hits']
+    res_from = 0
+    keep_looping = True
+    j_issns = []
+    a_issns = []
 
-    if results:
-        for result in results:
-            if result['_type'] == 'article':
-                a_issns += (result['fields']['index.issn'])
-            elif result['_type'] == 'journal':
-                j_issns += (result['fields']['index.issn'])
-    else:
-        keep_looping = False
-    res_from += RES_SIZE
+    while keep_looping:
+        query['from'] = res_from
+        resp = requests.get(SEARCH, data=json.dumps(query))
+        results = resp.json()['hits']['hits']
 
-journal_issns = set(j_issns)
-article_issns = set(a_issns)
+        if results:
+            for result in results:
+                if result['_type'] == 'article':
+                    a_issns += (result['fields']['index.issn'])
+                elif result['_type'] == 'journal':
+                    j_issns += (result['fields']['index.issn'])
+        else:
+            keep_looping = False
+        res_from += RES_SIZE
 
-print "Full list of journal ISSNs: {0}\t Set: {1}".format(len(j_issns),len(journal_issns))
-print "Full list of article ISSNs: {0}\t Set: {1}".format(len(a_issns),len(article_issns))
+    journal_issns = set(j_issns)
+    article_issns = set(a_issns)
 
-print "\nJournals without articles:"
-print journal_issns.difference(article_issns)
+    print "Full list of journal ISSNs: {0}\t Set: {1}".format(len(j_issns),len(journal_issns))
+    print "Full list of article ISSNs: {0}\t Set: {1}".format(len(a_issns),len(article_issns))
+
+    diff = journal_issns.difference(article_issns)
+    # print "\nJournals without articles:\n{0}".format(diff)
+
+    return list(diff)
--- retrieve_epmc_data.py
+++ retrieve_epmc_data.py
@@ -1,8 +1,35 @@
 # Get missing article data from EPMC
 
-import requests
+import requests, time
+from lxml import etree
+from find_empty_journals import find_empty_journals
 
 ISSN_SEARCH = 'http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=issn:'
+RESULT_TYPE = '&resulttype=core'
 
-resp = requests.get(ISSN_SEARCH + '0974-5181')
-print resp.text
+DELAY = 1   # Seconds
+
+empty_journals = find_empty_journals()
+print "Number of empty journals {0}".format(len(empty_journals))
+
+parser = etree.XMLParser(encoding='utf-8', recover=True, ns_clean=True)
+found_count = 0
+
+for j in empty_journals[:100]:
+    resp = requests.get(ISSN_SEARCH + j + RESULT_TYPE)
+    xml_tree = etree.fromstring(resp.text.encode('utf-8'), parser=parser)
+
+    hits = int(xml_tree.find('hitCount').text)
+    if hits > 0:
+        found_count += 1
+        print etree.tostring(xml_tree)
+
+    '''
+    for child in xml_tree.iterchildren():
+        if child.tag == 'hitCount':
+            if child.text == '0':
+                pass
+    '''
+    time.sleep(DELAY)
+
+print found_count
