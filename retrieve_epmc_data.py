b10cb3d710b36ef13a425fe09afcc25331e9d743
Steven Eardley <me@steveneardley.co.uk>
2014-09-17 00:04:23 +0100

    Expanded the retrieval to page through all results of the queries.

--- retrieve_epmc_data.py
+++ retrieve_epmc_data.py
@@ -11,22 +11,46 @@ PAGE = '&page={0}'
 params = RESULT_TYPE + FORMAT + PAGE
 DELAY = 1   # Seconds delay between requests to EPMC
 
-empty_journals = find_empty_journals()
+def check_epmc(journal_list):
+    found_count = 0
+    article_count_list = []
 
-print "\nQuerying Europe PubMed Central...\n"
-found_count = 0
-article_count_list = []
+    for j in journal_list[:20]:
+        page_index = 1
 
-for j in empty_journals[:20]:
-    page_index = 1
-    resp = requests.get(ISSN_SEARCH + j + params.format(page_index))
-    resp_json = resp.json()
+        resp = requests.get(ISSN_SEARCH + j + params.format(page_index))
+        resp_json = resp.json()
 
-    hits = resp_json['hitCount']
-    if hits > 0:
-        found_count += 1
-        article_count_list.append(hits)
+        # Check if we have anything to work with
+        hits = resp_json['hitCount']
+        if hits > 0:
+            found_count += 1
+            article_count_list.append(hits)
 
-    time.sleep(DELAY)
+            # get the first set of results
+            results = resp_json['resultList']['result']
 
-print "{0} Journals found in EPMC, covering a total of {1} articles, with a mean of {2} articles per journal.".format(found_count, sum(article_count_list), sum(article_count_list) / float(found_count))
+            while results:
+                # Deal with the previous set of results
+                handle_results(results)
+
+                # Get a new set of results, paging 25 at a time
+                page_index += 1
+                resp = requests.get(ISSN_SEARCH + j + params.format(page_index))
+                resp_json = resp.json()
+                results = resp_json['resultList']['result']
+                time.sleep(DELAY)
+
+        time.sleep(DELAY)
+
+    print "{0} Journals found in EPMC, covering a total of {1} articles, with a mean of {2} articles per journal.".format(found_count, sum(article_count_list), sum(article_count_list) / float(found_count))
+
+def handle_results(result_batch):
+    print result_batch[0]['id']
+
+if __name__ == '__main__':
+    print "Checking the index for articles without journals...\n"
+    empty_journals = find_empty_journals()
+
+    print "\nQuerying Europe PubMed Central...\n"
+    check_epmc(empty_journals)
