a193d53f5a90478a7946bd1e615d6f2a07135560
Steven Eardley <me@steveneardley.co.uk>
2015-03-06 14:19:36 +0000

    Steamlined the model somewhat and renamed a file.

--- article_models.py
+++ article_models.py
@@ -1,3 +1,13 @@
+import json, requests, uuid
+from copy import deepcopy
+from datetime import datetime
+import time
+
+import settings
+from bibjson_models import ArticleBibJSON
+
+# debugging
+import traceback, sys
 
 class DomainObject(object):
     __type__ = None # set the type on the model that inherits this
@@ -12,8 +22,7 @@ class DomainObject(object):
 
     @classmethod
     def target_whole_index(cls):
-        t = str(app.config['ELASTIC_SEARCH_HOST']).rstrip('/') + '/'
-        t += app.config['ELASTIC_SEARCH_DB'] + '/'
+        t = settings.ES_INDEX
         return t
 
     @classmethod
@@ -88,18 +97,11 @@ class DomainObject(object):
                 else:
                     return r
 
-            except requests.exceptions.ConnectionError:
+            except requests.ConnectionError:
                 attempt += 1
 
             # wait before retrying
             time.sleep((2**attempt) * back_off_factor)
-
-    def save_from_form(self,request):
-        newdata = request.json if request.json else request.values
-        for k, v in newdata.items():
-            if k not in ['submit']:
-                self.data[k] = v
-        self.save()
 
     @classmethod
     def bulk(cls, bibjson_list, idkey='id', refresh=False):
@@ -132,152 +134,6 @@ class DomainObject(object):
                 return cls(**out.json())
         except:
             return None
-
-    @classmethod
-    def pull_by_key(cls, key, value):
-        res = cls.query(q={"query":{"term":{key+app.config['FACET_FIELD']:value}}})
-        if res.get('hits',{}).get('total',0) == 1:
-            return cls.pull( res['hits']['hits'][0]['_source']['id'] )
-        else:
-            return None
-
-    @classmethod
-    def keys(cls,mapping=False,prefix=''):
-        # return a sorted list of all the keys in the index
-        if not mapping:
-            mapping = cls.query(endpoint='_mapping')[cls.__type__]['properties']
-        keys = []
-        for item in mapping:
-            if mapping[item].has_key('fields'):
-                for item in mapping[item]['fields'].keys():
-                    if item != 'exact' and not item.startswith('_'):
-                        keys.append(prefix + item + app.config['FACET_FIELD'])
-            else:
-                keys = keys + cls.keys(mapping=mapping[item]['properties'],prefix=prefix+item+'.')
-        keys.sort()
-        return keys
-
-    @staticmethod
-    def make_query(recid='', endpoint='_search', q='', terms=None, facets=None, should_terms=None, **kwargs):
-        '''
-        Generate a query object based on parameters but don't sent to
-        backend - return it instead. Must always have the same
-        parameters as the query method. See query method for explanation
-        of parameters.
-        '''
-        if recid and not recid.endswith('/'): recid += '/'
-        if isinstance(q,dict):
-            query = q
-            if 'bool' not in query['query']:
-                boolean = {'bool':{'must': [] }}
-                boolean['bool']['must'].append( query['query'] )
-                query['query'] = boolean
-            if 'must' not in query['query']['bool']:
-                query['query']['bool']['must'] = []
-        elif q:
-            query = {
-                'query': {
-                    'bool': {
-                        'must': [
-                            {'query_string': { 'query': q }}
-                        ]
-                    }
-                }
-            }
-        else:
-            query = {
-                'query': {
-                    'bool': {
-                        'must': [
-                            {'match_all': {}}
-                        ]
-                    }
-                }
-            }
-
-        if facets:
-            if 'facets' not in query:
-                query['facets'] = {}
-            for k, v in facets.items():
-                query['facets'][k] = {"terms":v}
-
-        if terms:
-            boolean = {'must': [] }
-            for term in terms:
-                if not isinstance(terms[term],list): terms[term] = [terms[term]]
-                for val in terms[term]:
-                    obj = {'term': {}}
-                    obj['term'][ term ] = val
-                    boolean['must'].append(obj)
-            if q and not isinstance(q,dict):
-                boolean['must'].append( {'query_string': { 'query': q } } )
-            elif q and 'query' in q:
-                boolean['must'].append( query['query'] )
-            query['query'] = {'bool': boolean}
-
-        # FIXME: this may only work if a term is also supplied above - code is a bit tricky to read
-        if should_terms is not None and len(should_terms) > 0:
-            for s in should_terms:
-                if not isinstance(should_terms[s],list): should_terms[s] = [should_terms[s]]
-                query["query"]["bool"]["must"].append({"terms" : {s : should_terms[s]}})
-
-        for k,v in kwargs.items():
-            if k == '_from':
-                query['from'] = v
-            else:
-                query[k] = v
-        # print json.dumps(query)
-        return query
-
-    @classmethod
-    def query(cls, recid='', endpoint='_search', q='', terms=None, facets=None, **kwargs):
-        '''Perform a query on backend.
-
-        :param recid: needed if endpoint is about a record, e.g. mlt
-        :param endpoint: default is _search, but could be _mapping, _mlt, _flt etc.
-        :param q: maps to query_string parameter if string, or query dict if dict.
-        :param terms: dictionary of terms to filter on. values should be lists.
-        :param facets: dict of facets to return from the query.
-        :param kwargs: any keyword args as per
-            http://www.elasticsearch.org/guide/reference/api/search/uri-request.html
-        '''
-        query = cls.make_query(recid, endpoint, q, terms, facets, **kwargs)
-        return cls.send_query(query, endpoint=endpoint, recid=recid)
-
-
-    @classmethod
-    def send_query(cls, qobj, endpoint='_search', recid='', retry=50):
-        '''Actually send a query object to the backend.'''
-        r = None
-        count = 0
-        exception = None
-        while count < retry:
-            count += 1
-            try:
-                if endpoint in ['_mapping']:
-                    r = requests.get(cls.target() + recid + endpoint)
-                else:
-                    r = requests.post(cls.target() + recid + endpoint, data=json.dumps(qobj))
-                break
-            except Exception as e:
-                exception = e
-            time.sleep(0.5)
-
-        if r is not None:
-            return r.json()
-        if exception is not None:
-            raise exception
-        raise Exception("Couldn't get the ES query endpoint to respond.  Also, you shouldn't be seeing this.")
-
-    def accessed(self):
-        if 'last_access' not in self.data:
-            self.data['last_access'] = []
-        try:
-            usr = current_user.id
-        except:
-            usr = "anonymous"
-        self.data['last_access'].insert(0, { 'user':usr, 'date':datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ") } )
-        r = requests.put(self.target() + self.data['id'], data=json.dumps(self.data))
 
     def delete(self):
         r = requests.delete(self.target() + self.id)
@@ -339,108 +195,9 @@ class DomainObject(object):
     def iterall(cls, page_size=1000, limit=None):
         return cls.iterate(deepcopy(all_query), page_size, limit)
 
-    def csv(self, multival_sep=','):
-        raise NotImplementedError
-
-    @classmethod
-    def prefix_query(cls, field, prefix, size=5):
-        # example of a prefix query
-        # {
-        #     "query": {"prefix" : { "bibjson.publisher" : "ope" } },
-        #     "size": 0,
-        #     "facets" : {
-        #       "publisher" : { "terms" : {"field" : "bibjson.publisher.exact", "size": 5} }
-        #     }
-        # }
-        if field.endswith(app.config['FACET_FIELD']):
-            # strip .exact (or whatever it's configured as) off the end
-            query_field = field[:field.rfind(app.config['FACET_FIELD'])]
-        else:
-            query_field = field
-
-        # the actual terms should come from the .exact version of the
-        # field - we are suggesting whole values, not fragments
-        facet_field = query_field + app.config['FACET_FIELD']
-
-        q = {
-            "query": {"prefix" : { query_field : prefix.lower() } },
-            "size": 0,
-            "facets" : {
-              field : { "terms" : {"field" : facet_field, "size": size} }
-            }
-        }
-
-        return cls.send_query(q)
-
-    @classmethod
-    def autocomplete(cls, field, prefix, size=5):
-        res = cls.prefix_query(field, prefix, size=size)
-        result = []
-        for term in res['facets'][field]['terms']:
-            # keep ordering - it's by count by default, so most frequent
-            # terms will now go to the front of the result list
-            result.append({"id": term['term'], "text": term['term']})
-        return result
-
-
-
 
 class Article(DomainObject):
     __type__ = "article"
-
-    @classmethod
-    def duplicates(cls, issns=None, publisher_record_id=None, doi=None, fulltexts=None, title=None, volume=None, number=None, start=None, should_match=None):
-        # some input sanitisation
-        issns = issns if isinstance(issns, list) else []
-        urls = fulltexts if isinstance(fulltexts, list) else [fulltexts] if isinstance(fulltexts, str) or isinstance(fulltexts, unicode) else []
-
-        q = DuplicateArticleQuery(issns=issns,
-                                    publisher_record_id=publisher_record_id,
-                                    doi=doi,
-                                    urls=urls,
-                                    title=title,
-                                    volume=volume,
-                                    number=number,
-                                    start=start,
-                                    should_match=should_match)
-        # print json.dumps(q.query())
-
-        res = cls.query(q=q.query())
-        articles = [cls(**hit.get("_source")) for hit in res.get("hits", {}).get("hits", [])]
-        return articles
-
-    @classmethod
-    def list_volumes(cls, issns):
-        q = ArticleVolumesQuery(issns)
-        result = cls.query(q=q.query())
-        return [t.get("term") for t in result.get("facets", {}).get("vols", {}).get("terms", [])]
-
-    @classmethod
-    def get_by_volume(cls, issns, volume):
-        q = ArticleQuery(issns=issns, volume=volume)
-        articles = cls.iterate(q.query(), page_size=1000)
-        return articles
-
-    @classmethod
-    def find_by_issns(cls, issns):
-        q = ArticleQuery(issns=issns)
-        articles = cls.iterate(q.query(), page_size=1000)
-        return articles
-
-    @classmethod
-    def delete_selected(cls, query=None, owner=None, snapshot=True):
-        if owner is not None:
-            from portality.models import Journal
-            issns = Journal.issns_by_owner(owner)
-            q = ArticleQuery(issns=issns)
-            query = q.query()
-
-        if snapshot:
-            articles = cls.iterate(query, page_size=1000)
-            for article in articles:
-                article.snapshot()
-
-        cls.delete_by_query(query)
 
     def bibjson(self):
         if "bibjson" not in self.data:
@@ -457,99 +214,6 @@ class Article(DomainObject):
         for h in hs:
             tuples.append((h.get("date"), ArticleBibJSON(h.get("bibjson"))))
         return tuples
-
-    def snapshot(self):
-        from portality.models import ArticleHistory
-
-        snap = deepcopy(self.data)
-        if "id" in snap:
-            snap["about"] = snap["id"]
-            del snap["id"]
-        if "index" in snap:
-            del snap["index"]
-        if "last_updated" in snap:
-            del snap["last_updated"]
-        if "created_date" in snap:
-            del snap["created_date"]
-
-        hist = ArticleHistory(**snap)
-        hist.save()
-
-        # FIXME: make this use the article history class
-        #snap = deepcopy(self.data.get("bibjson"))
-        #self.add_history(snap)
-
-    def add_history(self, bibjson, date=None):
-        """Deprecated"""
-        bibjson = bibjson.bibjson if isinstance(bibjson, ArticleBibJSON) else bibjson
-        if date is None:
-            date = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        snobj = {"date" : date, "bibjson" : bibjson}
-        if "history" not in self.data:
-            self.data["history"] = []
-        self.data["history"].append(snobj)
-
-    def is_in_doaj(self):
-        return self.data.get("admin", {}).get("in_doaj", False)
-
-    def set_in_doaj(self, value):
-        if "admin" not in self.data:
-            self.data["admin"] = {}
-        self.data["admin"]["in_doaj"] = value
-
-    def publisher_record_id(self):
-        return self.data.get("admin", {}).get("publisher_record_id")
-
-    def set_publisher_record_id(self, pri):
-        if "admin" not in self.data:
-            self.data["admin"] = {}
-        self.data["admin"]["publisher_record_id"] = pri
-
-    def upload_id(self):
-        return self.data.get("admin", {}).get("upload_id")
-
-    def set_upload_id(self, uid):
-        if "admin" not in self.data:
-            self.data["admin"] = {}
-        self.data["admin"]["upload_id"] = uid
-
-    def merge(self, old, take_id=True):
-        # this takes an old version of the article and brings
-        # forward any useful information that is needed.  The rules of merge are:
-        # - ignore "index" (it gets regenerated on save)
-        # - always take the "created_date"
-        # - any top level field that does not exist in the current item (esp "id" and "history")
-        # - in "admin", copy any field that does not already exist
-
-        # first thing to do is create a snapshot of the old record
-        old.snapshot()
-
-        # now go on and do the merge
-
-        # always take the created date
-        self.set_created(old.created_date)
-
-        # take the id
-        if self.id is None or take_id:
-            self.set_id(old.id)
-
-        # take the history (deprecated)
-        if len(self.data.get("history", [])) == 0:
-            self.data["history"] = deepcopy(old.data.get("history", []))
-
-        # take the bibjson
-        if "bibjson" not in self.data:
-            self.set_bibjson(deepcopy(old.bibjson()))
-
-        # take the admin if there isn't one
-        if "admin" not in self.data:
-            self.data["admin"] = deepcopy(old.data.get("admin", {}))
-        else:
-            # otherwise, copy any admin keys that don't exist on the current item
-            oa = old.data.get("admin", {})
-            for key in oa:
-                if key not in self.data["admin"]:
-                    self.data["admin"][key] = deepcopy(oa[key])
 
     def _generate_index(self):
         # the index fields we are going to generate
@@ -595,7 +259,8 @@ class Article(DomainObject):
         if jindex.get('country'):
             country = jindex.get('country')
         elif cbib.journal_country:
-            country = xwalk.get_country_name(cbib.journal_country)
+            pass
+            #country = xwalk.get_country_name(cbib.journal_country)
 
         # get the title of the license
         lic = cbib.get_journal_license()
@@ -649,36 +314,9 @@ class Article(DomainObject):
     def save(self):
         self._generate_index()
         super(Article, self).save()
-        if 'id' in self.data:
-            id_ = self.data['id'].strip()
-        else:
-            id_ = self.makeid()
-            self.data['id'] = id_
 
-        self.data['last_updated'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-
-        if 'created_date' not in self.data:
-            self.data['created_date'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-
-        attempt = 0
-        url = self.target() + self.data['id']
-        d = json.dumps(self.data)
-        while attempt <= retries:
-            try:
-                r = requests.post(url, data=d)
-                if r.status_code >= 400 and r.status_code < 500:
-                    # bad request, no retry
-                    print "bad request", r.json()
-                    traceback.print_stack(file=sys.stdout)
-                    break
-                elif r.status_code >= 500:
-                    print "server error", r.json()
-                    attempt += 1
-                else:
-                    return r
-
-            except requests.exceptions.ConnectionError:
-                attempt += 1
-
-            # wait before retrying
-            time.sleep((2**attempt) * back_off_factor)
+all_query = {
+    "query" : {
+        "match_all" : { }
+    }
+}
